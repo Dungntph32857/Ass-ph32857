5eccdb315bfe3b52bd4ca755d2b0da0630341619
Dungntph32857 <Dungntph32857@fpt.edu.vn>
2024-07-16 16:41:29 +0700

    init project

---
+++ resources/views/client/layouts/master.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Basic -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<!-- Mobile Metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Site Metas -->
+<title>Tech Blog - Stylish Magazine Blog Template</title>
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Site Icons -->
+<link rel="shortcut icon" href="/client/images/favicon.ico" type="image/x-icon" />
+<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+<!-- Design fonts -->
+    @include('client.layouts.partials.css')
+
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+        <header class="tech-header header">
+            @include('client.layouts.partials.header')
+        </header><!-- end market-header -->
+
+      
+
+        <section class="section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-9 col-md-12 col-sm-12 col-xs-12">
+                        @yield('content')
+                    </div><!-- end col -->
+
+                    @include('client.layouts.partials.content-right')
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </section>
+
+        <footer class="footer">
+            @include('client.layouts.partials.footer')
+        </footer><!-- end footer -->
+
+        <div class="dmtop">Scroll to Top</div>
+
+    </div><!-- end wrapper -->
+
+    <!-- Core JavaScript
+    ================================================== -->
+    <script src="/client/js/jquery.min.js"></script>
+    <script src="/client/js/tether.min.js"></script>
+    <script src="/client/js/bootstrap.min.js"></script>
+    <script src="/client/js/custom.js"></script>
+
+</body>
+
+</html>
---
+++ resources/views/client/layouts/partials/css.blade.php
@@ -0,0 +1,19 @@
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+
+<!-- Bootstrap core CSS -->
+<link href="/client/css/bootstrap.css" rel="stylesheet">
+
+<!-- FontAwesome Icons core CSS -->
+<link href="/client/css/font-awesome.min.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="/client/style.css" rel="stylesheet">
+
+<!-- Responsive styles for this template -->
+<link href="/client/css/responsive.css" rel="stylesheet">
+
+<!-- Colors for this template -->
+<link href="/client/css/colors.css" rel="stylesheet">
+
+<!-- Version Tech CSS for this template -->
+<link href="/client/css/version/tech.css" rel="stylesheet">
